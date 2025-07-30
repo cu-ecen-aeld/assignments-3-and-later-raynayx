51eb95cc76b8e4c64cdd8649f7a767720cd2127e
raynay <raynayx@raynayx.com>
2025-07-30 06:55:19 +0000

    test 1-1

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -15,9 +15,6 @@ if [ -d "${filesdir}" ]
 then
     echo "${filesdir} exists\n"
 
-    # X=$(ls $filesdir | wc -l)
-    # Y=$(grep -r "$searchstr" "$filesdir" | wc -l)
-
     X=$(grep -rF "$searchstr" "$filesdir" 2>/dev/null | wc -l)
 
     Y=$(find "$filesdir" -type f | wc -l)
