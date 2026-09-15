319fd01532ebbac03e8be807ea19c4b36954c2c2
raynay <raynayx@raynayx.com>
2026-09-15 16:11:26 +0200

    no cloning

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -31,7 +31,7 @@ cd "$OUTDIR"
 # 	git clone "${KERNEL_REPO}" #--depth 1 --single-branch --branch ${KERNEL_VERSION}
 # fi
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    ls .
+    echo "$(ls )"
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
